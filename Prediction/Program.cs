38afc019809489cd542f4dba89af8393d344ce81
Mostafizur Rahman <m_rahman_hridoy@yahoo.com>
2020-07-25 22:36:55 -0400

    initial

---
+++ Prediction/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Prediction
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter image file path: ");
+            string imageFilePath = Console.ReadLine();
+
+            MakePredictionRequest(imageFilePath).Wait();
+
+            Console.WriteLine("\n\nHit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        public static async Task MakePredictionRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid Prediction-Key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "d56c8981b11a4f1389757b5878dac33e");
+
+            // Prediction URL - replace this example URL with your valid Prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/1c6e5c77-42b8-4437-baa9-844a760f1a61/classify/iterations/Iteration1/image";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
